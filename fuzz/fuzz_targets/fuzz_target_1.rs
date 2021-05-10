9830cb9217a6f76760acb0966bf3bee0a5c74fa1
David Holroyd <dave@badgers-in-foil.co.uk>
2021-05-10 01:09:11 +0100

    Also fuzz builder api

--- fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -1,24 +1,19 @@
 #![no_main]
-#[macro_use] extern crate libfuzzer_sys;
-extern crate rtp_rs;
+use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(header) = rtp_rs::RtpReader::new(data) {
-        let _ = header.version();
-        let _ = header.padding();
-        if let Some(ext) = header.extension() {
-            let _ = ext.0;
-            let _ = ext.1;
-        }
-        let _ = header.csrc_count();
-        let _ = header.mark();
-        let _ = header.payload_type();
-        let _ = header.sequence_number();
-        let _ = header.timestamp();
-        let _ = header.ssrc();
-        let _ = header.payload().len();
-        let _ = header.extension();
-        for _ in header.csrc() {
+        format!("{:?}", header);
+        let b = header.create_builder();
+        let len = b.target_length();
+        let mut out = vec![0u8; len];
+        b.build_into(&mut out[..]).expect("build_into() failed");
+        if let Some(padding) = header.padding() {
+            let padding = padding as usize;
+            assert_eq!(data[0] & 0b1101_1111, out[0]);
+            assert_eq!(&data[1..data.len()-padding], &out[1..len]);
+        } else {
+            assert_eq!(&data[..len], &out[..]);
         }
     }
 });
